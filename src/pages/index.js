8cf9227065b47be6bd9dcd046bcaefaf697f70f0
Mateus de Sousa <mateusdesousa@localhost.localdomain>
2019-01-18 23:52:08 -0300

    inclusion of the library of maps

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import MapView from 'react-native-maps'
 
 import { View } from 'react-native';
-
 // import styles from './styles';
 
-const App = () => <View />;
+const App = () => (
+
+    <View style={{flex: 1 }}>
+        
+    </View>
+)
 
 export default App;
