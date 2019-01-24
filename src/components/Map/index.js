17b41c639c54f301692bbb87b8b2e6dd28b4317f
Mateus de Sousa <mateusdevsousa@gmail.com>
2019-01-24 15:49:39 -0300

    driven react-native-geocoding library and applying the origim label both the time it will take to the destination and the origm address

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -2,16 +2,21 @@ import React, { Component, Fragment } from 'react';
 import MapView, {Marker} from 'react-native-maps'
 import Search from '../Search/index'
 import Directions from '../Directions/index'
+import Geocode from 'react-native-geocoding'
 import {Platform, PixelRatio} from 'react-native'
 import { View } from 'react-native';
 import markerImage from '../../assets/IconeChegada.png'
 import {LocationBox, LocationText, LocationTimeBox, LocationTimeText, LocationTimeTextSmall, LocationTimeBox1} from './styles'
+import Geocoder from 'react-native-geocoding';
+
+Geocoder.init('AIzaSyDzqc1dyShJJRsnSTaYBTNtTd_gi2KrqG0')
 
 export default class Map extends Component {
     state = {
         region: null,
         destination: null,
-        duration: null
+        duration: null,
+        location: null
     }
 
     getPixelSize(pixels){
@@ -24,9 +29,13 @@ export default class Map extends Component {
     async componentDidMount(){
         navigator.geolocation.getCurrentPosition(
             //on success
-            ({coords: {latitude,longitude}}) => {
-                this.setState({region: {latitude,longitude, latitudeDelta: 0.0143,
-                    longitudeDelta: 0.0134}})
+            async ({coords: {latitude,longitude}}) => {
+                const response = await Geocoder.from({latitude, longitude})
+                const address = response.results[0].formatted_address
+                const location = address.substring(0, address.indexOf('-'))
+
+                this.setState({location, region: {latitude,longitude, latitudeDelta: 0.0143,
+                    longitudeDelta: 0.0134},})
             },
             //not success
             () => {
@@ -53,7 +62,7 @@ export default class Map extends Component {
     }
     
     render (){
-        const {region, destination, duration} = this.state
+        const {region, destination, duration, location} = this.state
         return(
             <View style={{flex: 1 }}>
                 <MapView
@@ -72,8 +81,8 @@ export default class Map extends Component {
                                     this.setState({duration: Math.floor(result.duration)})
                                     this.mapView.fitToCoordinates(result.coordinates, {
                                         edgePadding: {
-                                            right: this.getPixelSize(30),
-                                            left: this.getPixelSize(30),
+                                            right: this.getPixelSize(50),
+                                            left: this.getPixelSize(50),
                                             top: this.getPixelSize(50),
                                             bottom: this.getPixelSize(50)
                                         }
@@ -89,7 +98,7 @@ export default class Map extends Component {
                                         <LocationTimeText>{duration}</LocationTimeText>
                                         <LocationTimeTextSmall> MIN</LocationTimeTextSmall>
                                     </LocationTimeBox>
-                                    <LocationText>R. da Copaiba</LocationText>
+                                    <LocationText>{location}</LocationText>
                                 </LocationBox>
                             </Marker>
                         </Fragment>
