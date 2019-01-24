9b579f2c2013cdad2dbc007fee0d70dcc703e2bc
Mateus de Sousa <mateusdevsousa@gmail.com>
2019-01-24 19:40:09 -0300

    Adding value data screen to request the taxi

---
+++ src/components/Details/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import { Container, TypeTitle, TypeDescription, TypeImage, RequestButton, RequestButtonText } from './styles';
+import taxiImage from '../../assets/taxi.png' 
+
+
+export default class Details extends Component {
+  render() {
+    return <Container>
+        <TypeTitle>Popular</TypeTitle>
+
+        <TypeImage source={taxiImage} />
+        <TypeTitle>Taxi</TypeTitle>
+        <TypeDescription>R$6,50</TypeDescription>
+
+        <RequestButton onPress={() => {}}>
+            <RequestButtonText>
+                Solicitar Taxi
+            </RequestButtonText>
+        </RequestButton>
+        <RequestButton onPress={() => {}}>
+            <RequestButtonText>
+                Escolher outro destino
+            </RequestButtonText>
+        </RequestButton>
+    </Container>;    
+  }
+}
---
+++ src/components/Details/styles.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+    background: #FFF;
+    height: 280px;
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    shadow-color: #000;
+    shadow-offset: 0 0;
+    shadow-opacity: 0.2;
+    shadow-radius: 10;
+    elevation: 0.3;
+    border: 1px solid #DDD;
+    align-items: center;
+    padding: 20px;
+`
+
+export const TypeTitle = styled.Text`
+    font-size: 20px;
+    color: #222;
+`
+export const TypeDescription = styled.Text`
+    color: #666;
+    font-size: 14px;
+`
+export const RequestButtonText = styled.Text`
+    color: #FFF;
+    font-weight: bold;
+    font-size: 18px;
+    padding: 6px;
+`
+export const TypeImage = styled.Image`
+    height: 70px;
+    margin:10px 0;
+`
+export const RequestButton = styled.TouchableOpacity`
+    background: #666699;
+    justify-content: center;
+    align-items: center;
+    align-self: stretch;
+    margin: 3px;  
+`
